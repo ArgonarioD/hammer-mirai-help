19ae5229bb49a82c9214777b2053aa48de0aa605
ArgonarioD <arnordorian@sina.com>
2023-03-06 21:21:27 +0800

    make api normal dependency

--- hammer-mirai-help-plugin/build.gradle.kts
+++ hammer-mirai-help-plugin/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation("io.github.argonariod:hammer-mirai-utils:1.0.0")
     api(project(":hammer-mirai-help-api"))
+    asNormalDep(project(":hammer-mirai-help-api"))
 }
